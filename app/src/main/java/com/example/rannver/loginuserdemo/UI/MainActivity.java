54dc0b6d1f057f929aef2c458484ab7e25209912
Rannver <562763650@qq.com>
2017-01-17 21:56:53 +0800

    Initial commit

---
+++ app/src/main/java/com/example/rannver/loginuserdemo/UI/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.rannver.loginuserdemo.UI;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.rannver.loginuserdemo.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.btu_login_1)
+    Button btuLogin1;
+    @BindView(R.id.btu_login_2)
+    Button btuLogin2;
+    @BindView(R.id.tv_sinup_user)
+    TextView tvSinupUser;
+    @BindView(R.id.tv_change_pwd)
+    TextView tvChangePwd;
+    @BindView(R.id.ed_login_id)
+    EditText edLoginId;
+    @BindView(R.id.ed_login_pwd)
+    EditText edLoginPwd;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        String User_login_id = edLoginId.getText().toString();
+        String User_login_pwd = edLoginPwd.getText().toString();
+
+        //普通登录入口
+        btuLogin1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SignUpActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        //老年登录入口
+        btuLogin2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        //忘记密码入口
+        tvChangePwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        //注册入口
+        tvSinupUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/rannver/loginuserdemo/UI/SignUpActivity.java
@@ -0,0 +1,18 @@
+package com.example.rannver.loginuserdemo.UI;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.rannver.loginuserdemo.R;
+
+/**
+ * Created by Rannver on 2017/1/17.
+ */
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_activity);
+    }
+}
